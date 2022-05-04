9b0304bbbf8444ab50613168bbfa5eb586c313df
clark-pegg <clarkpegg@hotmail.com>
2022-05-04 16:48:58 -0400

    function definitions

--- src/server/player-actions.server.ts
+++ src/server/player-actions.server.ts
@@ -1,4 +1,4 @@
-import { Players } from "@rbxts/services";
+import { BadgeService, Players } from "@rbxts/services";
 import { Bag, Clicker, PlayerData, PlayerDataMap } from "server/player-data";
 import Remotes from "shared/remotes";
 
@@ -13,4 +13,43 @@ Players.PlayerAdded.Connect((player: Player) => {
       { price: 0, power: 1 },
     ],
   });
+});
+
+Remotes.Server.OnFunction("SubmitClick", (player: Player) => {
+  const data = PlayerDataMap.get(player);
+
+  if (!data) error("Couldn't find data!");
+
+  const newClicks = math.min(
+    data.clicks + data.equipped[1].power,
+    data.equipped[0].storage
+  );
+
+  PlayerDataMap.set(player, {
+    money: data.money,
+    clicks: newClicks,
+    bags: data.bags,
+    clickers: data.clickers,
+    equipped: data.equipped,
+  });
+
+  return newClicks;
+});
+
+Remotes.Server.OnFunction("SubmitSell", (player: Player) => {
+  const data = PlayerDataMap.get(player);
+
+  if (!data) error("Couldn't find data!");
+
+  const newMoney = data.money + data.clicks;
+
+  PlayerDataMap.set(player, {
+    money: newMoney,
+    clicks: 0,
+    bags: data.bags,
+    clickers: data.clickers,
+    equipped: data.equipped,
+  });
+
+  return newMoney;
 });
