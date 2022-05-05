082c42daa415934580b825758abe381bd0078b27
clark-pegg <clarkpegg@hotmail.com>
2022-05-04 22:44:25 -0400

    you can now see max clicks

--- src/client/main.client.tsx
+++ src/client/main.client.tsx
@@ -4,6 +4,7 @@ import Roact from "@rbxts/roact";
 
 const [money, updateMoney] = Roact.createBinding(0);
 const [clicks, updateClicks] = Roact.createBinding(0);
+const [maxClicks, updateMaxClicks] = Roact.createBinding(10);
 
 UserInputService.InputBegan.Connect(async (input, processed) => {
   if (processed) return;
@@ -15,13 +16,22 @@ UserInputService.InputBegan.Connect(async (input, processed) => {
 
 const MoneyClicksComponent = (
   money: Roact.Binding<number>,
-  clicks: Roact.Binding<number>
+  clicks: Roact.Binding<number>,
+  maxClicks: Roact.Binding<number>
 ) => (
   <frame Size={new UDim2(0.2, 0, 0.2, 0)}>
     <textlabel
       Size={new UDim2(1, 0, 0.5, 0)}
       Text={clicks.map((value) => {
-        return "Clicks: " + value;
+        return (
+          "Clicks: " +
+          value +
+          maxClicks
+            .map((value) => {
+              return " / " + value;
+            })
+            .getValue()
+        );
       })}
       Position={new UDim2(0, 0, 0, 0)}
     ></textlabel>
@@ -35,6 +45,30 @@ const MoneyClicksComponent = (
   </frame>
 );
 
+const SellComponent = () => (
+  <frame Size={new UDim2(0.2, 0, 0.2, 0)} Position={new UDim2(0.8, 0, 0.8, 0)}>
+    <textbutton
+      Size={new UDim2(1, 0, 1, 0)}
+      Text={"Sell"}
+      Event={{
+        MouseButton1Click: async () => {
+          updateMoney(await Remotes.Client.Get("SubmitSell").CallServerAsync());
+          updateClicks(0);
+        },
+      }}
+    ></textbutton>
+  </frame>
+);
+
+const ShopComponent = () => {};
+const InventoryComponent = () => {};
+
 const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui");
 
-Roact.mount(<screengui>{MoneyClicksComponent(money, clicks)}</screengui>, playerGui);
+Roact.mount(
+  <screengui>
+    {MoneyClicksComponent(money, clicks, maxClicks)}
+    {SellComponent()}
+  </screengui>,
+  playerGui
+);
